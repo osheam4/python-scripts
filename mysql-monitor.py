01a9edf752091db10cf14aa994a3267a8954dbb2
osheam4 <osheam4@gmail.com>
2014-09-16 13:03:58 +0000

    Edit MySQL monitor

--- mysql-monitor.py
+++ mysql-monitor.py
@@ -3,6 +3,28 @@
 from optparse import OptionParser
 import pymysql
 
+
+
+def getstats(opts):
+    results = {}
+    conn = pymysql.connect(host=opts.host, passwd=opts.passwd, user=opts.user, port=opts.port)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    cur.execute("SHOW GLOBAL STATUS")
+    data = cur.fetchall()
+    for item in data:
+        results[item['Variable_name']] = item['Value']
+    cur.execute("SHOW VARIABLES")
+    for item in data:
+        results[item['Variable_name']] = item['Value']
+    # TODO: get results of the following queries
+    #cur.execute("SHOW PROCESSLIST")
+    #cur.execute("SHOW MASTER LOGS")
+    #cur.execute("SHOW SLAVE STATUS")
+    #cur.execute("SHOW /*!50000 ENGINE*/ INNODB STATUS")
+    conn.close()
+    return results
+
+
 if __name__ == "__main__":
     parser = OptionParser(
         usage = "%prog [-o <Apache hostname or IP>]",
@@ -51,9 +73,9 @@ if __name__ == "__main__":
 
     (opts, args) = parser.parse_args()
     print "%s\t\t\t" % (opts)
-    conn = pymysql.connect(host=opts.host, passwd=opts.passwd, user=opts.user, port=opts.port)
-    cur = conn.cursor()
-    cur.execute("SHOW GLOBAL STATUS")
-    data = cur.fetchall()
-    print data
-    conn.close
+    results = getstats(opts)
+    print results.keys()[0]
+    print len(results)
+    for key, value in results.items():
+        if 'Threads_cached' in key:
+            print '%s\t\t%s' % (key, value)
