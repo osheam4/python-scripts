37ace7ac01a1c4627d299350b11389813c0a0a79
osheam4 <osheam4@gmail.com>
2014-11-04 16:56:33 +0000

    zabbix sender config
    zabbix sender configuration

--- apache-monitor.py
+++ apache-monitor.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
 
+import os
 from optparse import OptionParser
 import urllib
 
+zabbix_binary = '/usr/bin/zabbix_sender';
+zabbix_conf = '/etc/zabbix/zabbix_agentd.conf';
+zabbix_log = '/dev/null';
+
 results = {
-    'ConnsAsyncClosing': 0,
-    'Uptime': 0,
-    'IdleWorkers': 0,
-    'ConnsAsyncWriting': 0,
-    'Total Accesses': 0,
-    'Total kBytes': 0,
-    'BytesPerReq': 0,
-    'CPULoad': 0,
-    'BytesPerSec': 0,
-    'ReqPerSec': 0,
-    'ConnsTotal': 0,
-    'ConnsAsyncKeepAlive': 0,
-    'BusyWorkers': 0
+    #'ConnsAsyncClosing': 0,
+    #'Uptime': 0,
+    #'IdleWorkers': 0,
+    #'ConnsAsyncWriting': 0,
+    #'Total Accesses': 0,
+    #'Total kBytes': 0,
+    #'BytesPerReq': 0,
+    #'CPULoad': 0,
+    #'BytesPerSec': 0,
+    #'ReqPerSec': 0,
+    #'ConnsTotal': 0,
+    #'ConnsAsyncKeepAlive': 0,
+    #'BusyWorkers': 0
 }
 
 def fetchURL(url, user = None, passwd = None):
@@ -30,6 +35,10 @@ def fetchURL(url, user = None, passwd = None):
     finally:
         conn.close()
     return data
+
+def send_to_zabbix(results):
+    for key, value in results.iteritems():
+        os.system("%s -c %s -k %s -i %s" % (zabbix_binary, zabbix_conf, key, value))
 
 
 if __name__ == "__main__":
@@ -58,22 +67,43 @@ if __name__ == "__main__":
         default = 80,
         help = "Port to connect on. [default: %default]",
         )
+    parser.add_option(
+        "-l",
+        "--log",
+        action = "store_false",
+        dest = "log",
+        default = "False",
+        help = "Only log output, dont send to zabbix server"
+        )
 
     (opts, args) = parser.parse_args()
     opts.url = "http://%s:%s/server-status?auto" % (opts.host, opts.port)
-    print "%s\t\t\t%s" % (opts, args)
-
     data = fetchURL(opts.url)
-
     key = data.splitlines()
     for item in key:
         try:
             resultitem = item.split(': ')[1]
-            results[item.split(': ')[0]] = float(resultitem) if '.' in resultitem else int(resultitem)
+            results[item.split(': ')[0].replace(" ", "_").lower()] = float(resultitem) if '.' in resultitem else int(resultitem)
         except:
-            pass
+            if item.split(': ')[0] == "Scoreboard":
+                results['workers.open_slot'] = item.split(': ')[1].count('.')
+                results['workers.waiting'] = item.split(': ')[1].count('_')
+                results['workers.starting'] = item.split(': ')[1].count('S')
+                results['workers.reading'] = item.split(': ')[1].count('R')
+                results['workers.sending'] = item.split(': ')[1].count('W')
+                results['workers.keepalive'] = item.split(': ')[1].count('K')
+                results['workers.dns_lookup'] = item.split(': ')[1].count('D')
+                results['workers.closing'] = item.split(': ')[1].count('C')
+                results['workers.logging'] = item.split(': ')[1].count('L')
+                results['workers.gracefully_finishing'] = item.split(': ')[1].count('G')
+                results['workers.idle_cleanup'] = item.split(': ')[1].count('I')
     try:
         del results['Scoreboard']
     except KeyError:
         pass
-    print results
+    for key, value in results.iteritems():
+        key = key.replace(" ", "_").lower()
+    if opts.log == 'False':
+        send_to_zabbix(results)
+    else:
+        print results
